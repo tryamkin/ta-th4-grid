536052c02fe58d008134ecea02a9cdbe63ef458b
user.email <tryamkin@gmail.com>
2022-03-28 11:27:39 +0200

    test

--- src/main/java/com/epam/rd/autotasks/meetautocode/HelloAutocode.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/HelloAutocode.java
@@ -3,5 +3,6 @@ package com.epam.rd.autotasks.meetautocode;
 public class HelloAutocode {
     public static void main(String[] args) {
                 System.out.println("Hello, Autocode!");
+
     }
 }
